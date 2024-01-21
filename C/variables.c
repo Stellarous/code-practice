e5fd76899a19bf894061855f46203ddf877a3756
Ali Barmaki <108472998+Stellarous@users.noreply.github.com>
2024-01-21 11:11:16 +0000

    working with variables.

---
+++ C/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    //comment!
+    printf("Program...");
+
+    int num = 66;
+    char name = 'stel';
+    bool prop = false;
+
+    return 0;
+}
