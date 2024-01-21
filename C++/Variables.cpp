8920a8d517b422cd76849de23848e1d8bee6aa62
Ali Barmaki <108472998+Stellarous@users.noreply.github.com>
2024-01-21 11:18:53 +0000

    Working with variables.

---
+++ C++/Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //comment!
+
+    cout << "program...";
+
+    int num = 20;
+    char name = 'stella';
+    bool prop = true;
+
+    return 0;
+}
