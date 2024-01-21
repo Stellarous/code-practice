73ff28e5934640bdb9966301ca8ff7a5d0cfb065
Ali Barmaki <108472998+Stellarous@users.noreply.github.com>
2024-01-21 12:25:59 +0000

    Working with variables.

---
+++ Java/variables.java
@@ -0,0 +1,16 @@
+public class variables {
+
+    public static void main(String[] args) {
+
+        int number = 33;
+        System.out.println(number);
+
+        double num = 3.45;
+        System.out.println(num);
+
+        String name = "Stella";
+        char var = 'R';
+        System.out.println(name);
+        System.out.println(var);
+    }
+}
